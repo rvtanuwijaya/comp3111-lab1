554699dcb5f14dffd90fcb86367c6cb927b11a0c
Richarrdd <richardtanuwijaya22@gmail.com>
2022-09-16 15:34:33 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -4,8 +4,8 @@ public class Library {
 	
 	/* Add this function */
 	public static void main(String arg[]) {
-		//Library lib = new Library();
-		//if (lib.someLibraryMethod())
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
 		System.out.println("Hello, World!");
 	}
 		
